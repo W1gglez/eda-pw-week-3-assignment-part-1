c900de8fd4b4188d63f68b623c3148db59c26c15
lweigel <snr.wiggels@gmail.com>
2024-04-02 16:18:57 -0500

    Compelete required and stretch goals

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -10,19 +10,21 @@ console.log('Animals are: ', animalArray);
 // 1. TODO: Make an array `favoriteFoods` with some favorite foods.
 //    Be sure to console.log your array of foods to the console with
 //    a message, similar to the example above.
-
+let favoriteFoods = ['pizza', 'steak', 'cheesecake'];
+console.log('Some of my favorite foods are: ', favoriteFoods);
 
 // Array.length: A property that tells you how many items are in a given array.
 console.log('--- 2. Length of an array ---');
 
 // Example: How many animals are in the array?
-let numberOfAnimals = animalArray.length // 👈 Using the .length property!
+let numberOfAnimals = animalArray.length; // 👈 Using the .length property!
 console.log('Number of animals:', numberOfAnimals);
 
 // 2. TODO: Create a variable `numberOfFoods` and use the .length property
 //    to assign it the number value of how many items are inside `favoriteFoods`.
 //    Don't forget to console.log `numberOfFoods` to make sure your code worked!
-
+let numberOfFoods = favoriteFoods.length;
+console.log('Number of foods:', numberOfFoods);
 
 // Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -36,6 +38,8 @@ console.log('First animal is', firstAnimal);
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
 
+let secondAnimal = animalArray[1];
+console.log('Second animal is', secondAnimal);
 
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
 //      the "last" item in `animalArray`, using its array index.
@@ -43,10 +47,14 @@ console.log('First animal is', firstAnimal);
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
 
-// 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
+let lastAnimal = animalArray[3];
+console.log('Last animal is: ', lastAnimal);
+
+// 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length
 //       property, rather than the exact (hardcoded) index number of the last item.
 
-
+lastAnimal = animalArray[animalArray.length - 1];
+console.log('Last animal is: ', lastAnimal);
 
 // Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
@@ -61,6 +69,9 @@ console.log('Added an animal to end,', animalArray);
 //      Similar to above, add the `dessert` to the end of your `favoriteFoods`
 //      array. (How can you be 100% certain this worked? 🤔)
 
+let dessert = 'pecan pie';
+favoriteFoods.push(dessert);
+console.log('Added a dessert to end:', favoriteFoods);
 
 // Example: Add to the beginning of an array using the Array.unshift method.
 let animalToUnshift = 'walrus';
@@ -70,7 +81,9 @@ console.log(`Added an animal to beginning: ${animalArray}`);
 // 4.b. TODO: Create a variable `entree` and assign it a string value of
 //      an entree that you enjoy.
 //      Add the `entree` to the beginning of your `favoriteFoods` array.
-
+let entree = 'lasagna';
+favoriteFoods.unshift(entree);
+console.log('Added an entree to the beginning: ', favoriteFoods);
 
 // Example: Remove the last animal by using the Array.pop method.
 let removedAnimal = animalArray.pop();
@@ -86,7 +99,9 @@ let colors = ['orange', 'yellow', 'green', 'teal'];
 //      Don't forget! Use console.log to verify that the `colors`
 //      array only contains three items now, AND to verify that `lastColor`
 //      contains the value "teal".)
-
+let lastColor = colors.pop();
+console.log('Removed the last color: ', lastColor);
+console.log('The colors are now: ', colors);
 
 // Example: Remove the first animal using the Array.shift method.
 removedAnimal = animalArray.shift();
@@ -95,10 +110,10 @@ console.log('The animals are now', animalArray);
 
 // 4.d. TODO: Remove the first value from the `colors` array and store it
 //      inside a new variable called `firstColor`.
-//      To verify that your code worked, console.log `firstColor`!  
-
-
-
+//      To verify that your code worked, console.log `firstColor`!
+let firstColor = colors.shift();
+console.log('Removed the first color: ', firstColor);
+console.log('The colors are now ', colors);
 
 // STRETCH GOALS:
 
@@ -106,11 +121,13 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 
 // 5.a (STRETCH) TODO: Replace the second gem with 'Tourmaline'.
 //      Remember to console.log the updated array.
-
+gems[1] = 'Tourmaline';
+console.log(gems);
 
 // 5.b (STRETCH) TODO: Sort your gems array in reverse alphabetical order.
 //     Please console.log the sorted array.
-
+gems.sort().reverse();
+console.log(gems);
 
 // 5.c (STRETCH) TODO: Create a variable `gemString` and assign it a string value
 //     that's made up of each value from the `gems` array, but separated by the
@@ -119,24 +136,27 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 //       "Emerald and Diamond and Tourmaline and Topaz"
 //     🔥 You'll need to research the .join Array method. 🔥
 //     Don't forget to console.log `gemString`...
+let gemString = gems.join(' and ');
+console.log(gemString);
 
-
-// 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines 
+// 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines
 //     the gems array with the colors array.
 //     It should look something like:
 //       ['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond', 'yellow', 'green']
 //     Now, console.log the new array.
-
-
-
+let colorfulGems = gems.concat(colors);
+console.log(colorfulGems);
 
 // DO NOT MODIFY
 // Used for automated testing
 try {
   module.exports = {
-    favoriteFoods: typeof favoriteFoods !== 'undefined' ? favoriteFoods : undefined,
-    numberOfFoods: typeof numberOfFoods !== 'undefined' ? numberOfFoods : undefined,
-    secondAnimal: typeof secondAnimal !== 'undefined' ? secondAnimal : undefined,
+    favoriteFoods:
+      typeof favoriteFoods !== 'undefined' ? favoriteFoods : undefined,
+    numberOfFoods:
+      typeof numberOfFoods !== 'undefined' ? numberOfFoods : undefined,
+    secondAnimal:
+      typeof secondAnimal !== 'undefined' ? secondAnimal : undefined,
     lastAnimal: typeof lastAnimal !== 'undefined' ? lastAnimal : undefined,
     dessert: typeof dessert !== 'undefined' ? dessert : undefined,
     entree: typeof entree !== 'undefined' ? entree : undefined,
@@ -144,7 +164,8 @@ try {
     firstColor: typeof firstColor !== 'undefined' ? firstColor : undefined,
     gems: typeof gems !== 'undefined' ? gems : undefined,
     gemString: typeof gemString !== 'undefined' ? gemString : undefined,
-    colorfulGems: typeof colorfulGems !== 'undefined' ? colorfulGems : undefined,
+    colorfulGems:
+      typeof colorfulGems !== 'undefined' ? colorfulGems : undefined,
     colors: typeof colors !== 'undefined' ? colors : undefined,
   };
 } catch (e) {
